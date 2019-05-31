bcf217e0c6220eff63192be66011905b8566ba8a
SanaHajji <47304748+SanaHajji@users.noreply.github.com>
2019-05-31 13:09:41 +0100

    Update hello.c

--- hello.c
+++ hello.c
@@ -3,6 +3,7 @@ int main()
 {
    // printf() displays the string inside quotation
    printf("Hello, World!");
-   printf("i'm ready");
+   printf("i'm ready ");
+   printf("i'm ready for jenkins ");
    return 0;
 }
